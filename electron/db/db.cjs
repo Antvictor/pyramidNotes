cd699d8ce9f182935cd69260ddf9ed29aae606dd
AntVictor <AntVictorPlus@gmail.com>
2026-03-29 11:48:20 +0800

    init: 引入libsimple分词

--- electron/db/db.cjs
+++ electron/db/db.cjs
@@ -2,6 +2,8 @@ const Database = require('better-sqlite3');
 const { app, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
+const { getBasePath } = require('../window/window.cjs');
 
 let db = null;
 
@@ -16,6 +18,10 @@ function initializeDatabase() {
         fs.mkdirSync(path.dirname(dbPath), { recursive: true });
     }
     db = new Database(dbPath);
+
+
+    // 加载插件
+    loadPlugins(db);
 
     db.exec(`
         create table if not exists notes(
@@ -88,6 +94,66 @@ function initializeDatabase() {
             return null;
         }
     });
+
+    function loadPlugins(db) {
+        db.pragma('journal_mode = WAL');
+
+        // 允许加载扩展
+        db.loadExtension = db.loadExtension || db.loadExtension;
+        // 加载插件
+        loadFts5(db);
+    };
+
+    function getRealArch() {
+        if (process.platform === 'darwin') {
+            try {
+                return execSync('uname -m').toString().trim();
+            } catch { }
+        }
+
+        if (process.platform === 'win32') {
+            return (
+                process.env.PROCESSOR_ARCHITEW6432 ||
+                process.env.PROCESSOR_ARCHITECTURE
+            );
+        }
+
+        return process.arch;
+    }
+
+    function resolveExt() {
+        const map = {
+            win32: 'dll',
+            darwin: 'dylib',
+            linux: 'so',
+        };
+        return map[process.platform];
+    }
+
+    function normalizeArch(arch) {
+        if (arch === 'x86_64' || arch === 'AMD64') return 'x64';
+        if (arch === 'arm64') return 'arm64';
+        return 'x86';
+    }
+
+    function loadFts5(db) {
+        const arch = normalizeArch(getRealArch());
+        const ext = resolveExt();
+
+        const basePath = getBasePath();
+        // 打包后资源目录（关键）
+
+        const filePath = path.join(
+            basePath,
+            'sqlite-plugins',
+            `${process.platform}-${arch}`,
+            `libsimple.${ext}`
+        );
+
+        console.log('Load extension:', filePath);
+
+        db.loadExtension(filePath);
+    }
 }
 
 module.exports = { initializeDatabase, getDb }
--- electron/window/window.cjs
+++ electron/window/window.cjs
@@ -1,4 +1,4 @@
-const { BrowserWindow } = require("electron");
+const { BrowserWindow, app } = require("electron");
 const path = require("path");
 
 function createWindow() {
@@ -10,8 +10,26 @@ function createWindow() {
     }
   });
 
-  win.loadURL("http://localhost:5173");
+  if (app.isPackaged) {
+    // 生产环境：加载打包后的前端文件
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  } else {
+    // 开发环境：加载 Vite dev server
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  }
+
   return win;
 }
 
-module.exports = { createWindow };
+function getBasePath() {
+  if (app.isPackaged) {
+    // 打包后
+    return process.resourcesPath;
+  } else {
+    // 开发环境
+    return path.join(__dirname, '..'); 
+    // ⚠️ 根据你项目结构调整
+  }
+}
+module.exports = { createWindow, getBasePath };
