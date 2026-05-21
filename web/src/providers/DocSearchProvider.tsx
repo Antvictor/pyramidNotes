99921ab2ec5282ac2c9bfb229441fe5534a16ad4
Antvictor <17560726090@163.com>
2026-05-21 17:04:35 +0800

    style: stub DocSearchProvider for PyramidNotes

--- web/src/providers/DocSearchProvider.tsx
+++ web/src/providers/DocSearchProvider.tsx
@@ -1,20 +1,6 @@
-import { createContext, FC, ReactNode, useContext } from 'react'
+import type { FC, ReactNode } from 'react'
 
-import { DocSearch } from '@/utils/types'
-
-const docSearchCtx = createContext<DocSearch>({
-  appId: '',
-  apiKey: '',
-  indexName: '',
-})
-
-export const useDocSearch = () => useContext(docSearchCtx)
-
-export const DocSearchProvider: FC<{
-  children: ReactNode
-  docSearch: DocSearch
-}> = ({ children, docSearch }) => {
-  return (
-    <docSearchCtx.Provider value={docSearch}>{children}</docSearchCtx.Provider>
-  )
+// PyramidNotes doesn't use Algolia DocSearch, so this is a stub provider
+export const DocSearchProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  return <>{children}</>
 }
